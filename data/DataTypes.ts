9565c9bc3f2c82cc4dcd795660373405dca6cc89
devrusty <rguillory246@gmail.com>
2022-10-02 20:19:09 -0500

    added values

--- data/DataTypes.ts
+++ data/DataTypes.ts
@@ -1,8 +1,8 @@
 enum GoodDBDataType {
-    int32,
-    int64,
-    string,
-    array,
-    boolean
+    int32 = "int32",
+    int64 = "int64",
+    string = "string",
+    array = "array",
+    boolean = "boolean"
 }
 export default GoodDBDataType
